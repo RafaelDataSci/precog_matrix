6bdf87f93aa9da0f4028fae7b99de1c48ae9b605
Andre Montanari <andrevallemontanari@hotmail.com>
2024-06-15 19:29:06 -0300

    no redundancy, cache and fix

--- precog-matrix-fast-api/app/streamlit_app.py
+++ precog-matrix-fast-api/app/streamlit_app.py
@@ -1,21 +1,11 @@
 import streamlit as st
 import folium
 from streamlit_folium import folium_static
-from geopy.geocoders import GoogleV3
 import requests
 from datetime import datetime
 
 # Streamlit configuration
 st.set_page_config(page_title="Crime Prediction", page_icon="🔍", layout="wide")
-
-# Function to geocode address
-def geocode_address(address):
-    geolocator = GoogleV3(api_key="GOOGLE_API_KEY")
-    location = geolocator.geocode(address)
-    if location:
-        return location.latitude, location.longitude
-    else:
-        return None, None
 
 # Function to call the prediction API
 def predict_crime(address, crime_date):
@@ -37,19 +27,15 @@ crime_time = st.time_input("Crime Time", datetime.now().time())
 
 # Prediction button
 if st.button("Predict Crime"):
-    lat, lon = geocode_address(address)
-    if lat is not None and lon is not None:
-        combined_datetime = datetime.combine(crime_date, crime_time)
-        prediction = predict_crime(address, combined_datetime)
-        if prediction:
-            st.write("Crime Prediction Probabilities:")
-            crime_labels = ["AUTO THEFT", "ASSAULT", "ROBBERY", "THEFT OVER", "BREAK AND ENTER", "HOMICIDE"]
-            for crime, prob in zip(crime_labels, prediction[0]):
-                st.write(f"{crime}: {prob:.2f}")
-        else:
-            st.error("Error fetching prediction. Please try again.")
+    combined_datetime = datetime.combine(crime_date, crime_time)
+    prediction = predict_crime(address, combined_datetime)
+    if prediction:
+        st.write("Crime Prediction Probabilities:")
+        crime_labels = ["AUTO THEFT", "ASSAULT", "ROBBERY", "THEFT OVER", "BREAK AND ENTER", "HOMICIDE"]
+        for crime, prob in zip(crime_labels, prediction[0]):
+            st.write(f"{crime}: {prob:.2f}")
     else:
-        st.error("Could not geocode the provided address.")
+        st.error("Error fetching prediction. Please try again.")
 
 # Interactive map
 st.subheader("Click on the map to select a location")
